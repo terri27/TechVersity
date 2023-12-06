41ca5bda276aa067aa45afc270a332581cf8f8ad
Angel Martinez <angelmarti18@icloud.com>
2023-12-06 14:27:23 -0500

    Correcion en archivo de base de datos admin/course/course.php linea 84

--- DB_connection.php
+++ DB_connection.php
@@ -3,7 +3,7 @@
 $sName = "localhost";
 $uName = "root";
 $pass  = "";
-$db_name = "softwaregestion";
+$db_name = "softwaregestionescolar";
 
 try {
 	$conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
